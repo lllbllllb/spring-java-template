20564ef67a7b0c5689a5d640994fec2af5dc3bdb
lll <enakes@gmail.com>
2026-07-11 11:09:39 +0200

    set useOptionalForNullableProperties: false

--- template/models.js
+++ template/models.js
@@ -11,7 +11,7 @@ const {getDefaultJavaPackage, getFilePackage} = require('../lib/util.js');
 const {addFile} = require('../lib/state.js');
 
 const javaGenerator = new JavaGenerator({
-    useOptionalForNullableProperties: true,
+    useOptionalForNullableProperties: false,
     processorOptions: {
         interpreter: {
             ignoreAdditionalProperties: true
